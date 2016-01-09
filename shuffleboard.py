eaf431363975119733b0f1fc19a14840866d0b81
efischer0119 <efischer0119@gmail.com>
2016-01-09 14:56:17 -0500

    WIP list shows in dialog

--- shuffleboard.py
+++ shuffleboard.py
@@ -1,7 +1,16 @@
 import xbmcaddon
 import xbmcgui
+import xbmc
+import json
 
 addon       = xbmcaddon.Addon()
 addonname   = addon.getAddonInfo('name')
 
-xbmcgui.Dialog().ok(addonname, "Hello World!")
+ShowQueryCmd = '{"jsonrpc": "2.0", "method": "VideoLibrary.GetTVShows", "properties": ["title"], "sort": { "order": "ascending", "method": "label" }, "id": "libTvShow" }'
+shows = xbmc.executeJSONRPC(ShowQueryCmd)
+
+xbmc.log("Got some shows!", xbmc.LOGDEBUG)
+xbmc.log("{} of them, to be exact!".format(len(shows)), xbmc.LOGDEBUG)
+xbmc.log("Here's the response: {}".format(json.loads(shows)), xbmc.LOGDEBUG)
+
+xbmcgui.Dialog().ok(addonname, shows)
